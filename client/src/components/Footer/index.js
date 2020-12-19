089b6379bed99988a41770c2a384d1639fa6a7b2
Alexandria Robertson <67127042+alexemrob@users.noreply.github.com>
2020-12-19 09:00:02 -0600

    remove body tag

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,13 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Footer() {
     return (
-        <body className="d-flex flex-column h-100">
+        <div className="d-flex flex-column h-100">
             <footer className="footer fixed-bottom ">
                 <div className="container d-flex justify-content-center">
                     <span className="text-muted">Client Catalogue</span>
                 </div>
             </footer>
-        </body>
+        </div>
     );
 }
 
